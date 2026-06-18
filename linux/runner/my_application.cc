2ed8c7d14959946656ef73a9f348114bdf9a87d5
Osama Sanad <osamasanad2@example.com>
2026-06-19 00:39:29 +0300

    تغيير اسم التطبيق إلى 'إبراهيم رفيقك الديني'

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -45,11 +45,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "ibrahim");
+    gtk_header_bar_set_title(header_bar, "إبراهيم رفيقك الديني");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "ibrahim");
+    gtk_window_set_title(window, "إبراهيم رفيقك الديني");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
